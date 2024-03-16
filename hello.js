cf66aabce1536e10428210e3f84cd52eed1e2d03
merryviji <merryviji2k03@gmail.com>
2024-03-16 14:51:44 -0500

    first commit

---
+++ hello.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sayGoodbye = exports.sayHello = void 0;
+const helloMessage = "Hello, World";
+const goodbyeMessage = "GoodBye, World";
+function sayHello() {
+    console.log(helloMessage);
+}
+exports.sayHello = sayHello;
+function sayGoodbye() {
+    console.log(goodbyeMessage);
+}
+exports.sayGoodbye = sayGoodbye;
+//# sourceMappingURL=hello.js.map
